0e5b5e712abafad38631531553822eea7fcd28b6
Joshua Taylor <49791155+jdtaylor311@users.noreply.github.com>
2021-05-03 10:36:35 -0400

    first commit

---
+++ section2/arrays/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int A[5] = {0,1,2,3,4};
+
+    for (int i = 0; i < 5; i++){
+        printf("%d ", A[i]);
+        printf("\n");
+    }
+    printf("this is done now");
+    
+    return 0;
+}
