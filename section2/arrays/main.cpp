c4d9aec5966857563575a42f7cf3bbaf50c15aa8
Joshua Taylor <49791155+jdtaylor311@users.noreply.github.com>
2021-05-03 11:38:52 -0400

    converting from printf to cout

--- section2/arrays/main.cpp
+++ section2/arrays/main.cpp
@@ -2,15 +2,19 @@
 
 using namespace std;
 
-int main(){
+int main() {
+  int A[10] = {0, 1, 2, 3, 4};
 
-    int A[5] = {0,1,2,3,4};
+  // Can set values in array like below...
+  // A[0] = 1;
+  // A[1] = 2;
+  // A[2] = 3;
 
-    for (int i = 0; i < 5; i++){
-        printf("%d ", A[i]);
-        printf("\n");
-    }
-    printf("this is done now");
-    
-    return 0;
+  for (int i = 0; i < 5; i++) {
+    cout << A[i] << endl;
+  }
+  cout << sizeof(A) << endl;
+
+
+  return 0;
 }
