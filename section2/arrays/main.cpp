796d9de7f536778991505bc2b0b1488fce066f66
Joshua Taylor <49791155+jdtaylor311@users.noreply.github.com>
2021-05-03 11:58:48 -0400

    adding input for size of array and input for each element in array

--- section2/arrays/main.cpp
+++ section2/arrays/main.cpp
@@ -3,7 +3,14 @@
 using namespace std;
 
 int main() {
-  int A[10] = {0, 1, 2, 3, 4};
+  int n;
+  cout<<"What size array do you want?"<<endl;
+  cin >> n;
+  int A[n];
+  for(int i = 0; i < n; i++){
+    cout<<"Enter the value for A[x]: "<<endl;
+    cin >> A[i];
+  }
 
   // Can set values in array like below...
   // A[0] = 1;
@@ -12,6 +19,10 @@ int main() {
 
   for (int i = 0; i < 5; i++) {
     cout << A[i] << endl;
+  }
+
+  for(int x:A){
+    cout<<x<<endl;
   }
   cout << sizeof(A) << endl;
 
