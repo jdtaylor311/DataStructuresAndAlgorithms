39bd89a1172e3b61a6928c93800013183962eab4
Joshua Taylor <49791155+jdtaylor311@users.noreply.github.com>
2021-05-16 22:33:37 -0400

    looked at static and global variables in recursion & created a tree recursion function which has a time complexity of O(2^n) and space complexity of O(n)

---
+++ section5/staticAndGlobalVariablesInRecursion/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int fun(int n) {
+  static int x = 0;
+  if (n > 0) {
+    x++;
+    return fun(n - 1) + x;
+  }
+  return 0;
+}
+
+int main() {
+  int a = 15;
+  printf("%d ", fun(a));
+
+  return 0;
+}
